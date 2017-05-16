ba12d56115ace4c7fdff013b5af448c204a2bf05
tanner0101 <me@tanner.xyz>
2017-05-17 00:01:20 +0100

    2.0 default

--- deploy.sh
+++ deploy.sh
@@ -12,4 +12,4 @@ mkdir -p build;
 mv 2.0/site build/2.0;
 mv 1.5/.couscous/generated build/1.5;
 
-echo "<meta http-equiv=\"refresh\" content=\"0; url=/1.5/\">" > build/index.html;
+echo "<meta http-equiv=\"refresh\" content=\"0; url=/2.0/\">" > build/index.html;
