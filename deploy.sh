c2932e1eb1eed1b93c86dbb63e953a2a0a2eb001
tanner0101 <me@tanner.xyz>
2017-05-16 12:21:08 +0100

    file switch

---
+++ deploy.sh
@@ -0,0 +1,15 @@
+cd 2.0;
+mkdocs build;
+cd ..;
+
+cd 1.5;
+couscous generate;
+cd ..;
+
+rm -rf build
+mkdir -p build;
+
+mv 2.0/site build/2.0;
+mv 1.5/.couscous/generated build/1.5;
+
+echo "<meta http-equiv=\"refresh\" content=\"0; url=/2.0/\">" > build/index.html;
