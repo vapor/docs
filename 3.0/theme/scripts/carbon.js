36bcab4451e56f9d3e405e9a2485eb0240b09660
Tanner Nelson <me@tanner.xyz>
2018-12-05 17:07:25 -0500

    remove debug print + cleanup css

--- 3.0/theme/scripts/carbon.js
+++ 3.0/theme/scripts/carbon.js
@@ -1,6 +1,5 @@
 // data-md-component="toc"
 document.addEventListener("DOMContentLoaded", function(event) {
-	console.log(document);
 	var toc_inner = document.querySelectorAll('[data-md-component=toc] .md-sidebar__inner')[0];
 	var script = document.createElement("script");
 	script.src = '//cdn.carbonads.com/carbon.js?serve=CK7DT2QW&placement=vaporcodes';
