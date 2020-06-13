0debb060425ac372517ac61ac5d96e516416d3cc
Tanner Nelson <me@tanner.xyz>
2020-06-13 17:12:52 +0000

    update build

--- build.sh
+++ build.sh
@@ -11,7 +11,7 @@ mkdocs build;
 cd ..;
 
 cd 1.5;
-# couscous generate;
+couscous generate;
 cd ..;
 
 rm -rf site
@@ -23,7 +23,7 @@ cd .. && pip install leaf-pygment/dist/leaf-0.1.0-dev.tar.gz
 mv 2.0/site site/2.0;
 mv 3.0/site site/3.0;
 mv 4.0/site site/4.0;
-# mv 1.5/.couscous/generated site/1.5;
+mv 1.5/.couscous/generated site/1.5;
 
 echo "<meta http-equiv=\"refresh\" content=\"0; url=/4.0/\">" > site/index.html;
 
