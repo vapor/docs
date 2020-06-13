35168e52b9d813e8833b7ebf89c2ad94e665d0dd
tanner0101 <me@tanner.xyz>
2020-06-13 12:53:03 -0400

    build fix

--- build.sh
+++ build.sh
@@ -14,16 +14,16 @@ cd 1.5;
 # couscous generate;
 cd ..;
 
-rm -rf build
-mkdir -p build;
+rm -rf site
+mkdir -p site;
 
 cd leaf-pygment && ./compile.sh
 cd .. && pip install leaf-pygment/dist/leaf-0.1.0-dev.tar.gz
 
-mv 2.0/site build/2.0;
-mv 3.0/site build/3.0;
-mv 4.0/site build/4.0;
-# mv 1.5/.couscous/generated build/1.5;
+mv 2.0/site site/2.0;
+mv 3.0/site site/3.0;
+mv 4.0/site site/4.0;
+# mv 1.5/.couscous/generated site/1.5;
 
 echo "<meta http-equiv=\"refresh\" content=\"0; url=/4.0/\">" > build/index.html;
 
