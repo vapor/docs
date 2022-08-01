a2c3533b845b5a3cf80f3700ffbebd275818a0b2
Tim Condon <0xTim@users.noreply.github.com>
2022-08-01 17:26:29 +0100

    Fix Carbon

--- theme/scripts/carbon.js
+++ theme/scripts/carbon.js
@@ -1,6 +1,6 @@
 // data-md-component="toc"
 document.addEventListener("DOMContentLoaded", function(event) {
-	var toc_inner = document.querySelectorAll('[data-md-component=toc] .md-sidebar__inner')[0];
+	var toc_inner = document.querySelectorAll('[data-md-component=toc]')[0];
 	var script = document.createElement("script");
 	script.src = '//cdn.carbonads.com/carbon.js?serve=CK7DT2QW&placement=vaporcodes';
 	script.type = 'text/javascript';
