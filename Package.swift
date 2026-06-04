2c7a78330b2c4f783e47e4d85263fec64dfae669
Tim <0xtimc@gmail.com>
2026-06-04 18:26:22 +0100

    Tidy up manifest

--- Package.swift
+++ Package.swift
@@ -7,11 +7,7 @@ let package = Package(
         .macOS(.v13)
     ],
     dependencies: [
-        // Kiln is fetched from GitHub (used by CI, Docker and deployment).
-        // For local development against a checkout of Kiln, comment this out
-        // and use the path dependency below instead.
         .package(url: "https://github.com/brokenhandsio/kiln.git", branch: "main"),
-        // .package(path: "../../BH/kiln"),
     ],
     targets: [
         .executableTarget(
