2745a0ba49c72a88d0154d75e2560b3553649966
Tim <0xtimc@gmail.com>
2026-07-04 19:30:57 +0100

    Use release of design

--- Package.swift
+++ Package.swift
@@ -7,12 +7,8 @@ let package = Package(
         .macOS(.v13)
     ],
     dependencies: [
-        // Local path dep while the shared-layer/head features are unreleased; swap
-        // back to the tagged release once Kiln + the design package are published.
-        // .package(url: "https://github.com/brokenhandsio/kiln.git", from: "1.3.1"),
-        .package(path: "../../BrokenHands/kiln"),
-        // Shared Vapor design templates (footer/head), consumed as a Kiln theme layer.
-        .package(path: "../design"),
+        .package(url: "https://github.com/brokenhandsio/kiln.git", from: "1.4.1"),
+        .package(url: "https://github.com/vapor/design.git", branch: "main"),
     ],
     targets: [
         .executableTarget(
